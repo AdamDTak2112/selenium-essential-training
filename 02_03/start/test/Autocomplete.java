e8cfe5489efe736edb668d812c6e3f9a6dc598b4
Adam Tak <adam.tak2112@gmail.com>
2022-06-16 12:21:04 -0600

    finish 02_02 and 02_03

--- 02_03/start/test/Autocomplete.java
+++ 02_03/start/test/Autocomplete.java
@@ -6,11 +6,15 @@ import org.openqa.selenium.By;
 public class Autocomplete {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/adam.tak/Desktop/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+
+        autocomplete.sendKeys("2975 Porter Ave, Ogden UT");
 
         driver.quit();
     }
